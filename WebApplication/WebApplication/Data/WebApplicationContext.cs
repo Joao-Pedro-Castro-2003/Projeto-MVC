68e19dd91feced9c7305aaa88950c931f9807219
Joao-Pedro-Castro-2003 <jp.castro0509@gmail.com>
2024-09-06 18:01:18 -0300

    fix: Configs

---
+++ WebApplication/WebApplication/Data/WebApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class WebApplicationContext : DbContext
+    {
+        public WebApplicationContext(DbContextOptions<WebApplicationContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<UsuarioModel> Usuario { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Common;
+
+namespace WebApplication.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
